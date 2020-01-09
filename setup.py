d094311fababf88d03bae589fd49770bda843438
Laxmi30 <46951085+Laxmi30@users.noreply.github.com>
2020-01-10 02:50:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
-pip install
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-Laxmi30", # Replace with your own username
+    version="0.0.1",
+    author="laxmi",
+    author_email="podililaxmi30@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Laxmi30/python_demo_package.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
